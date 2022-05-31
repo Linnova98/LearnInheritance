e7d345b6cacc3672fa234b735ee78c2a01d91bda
Linnova <92028792+Linnova98@users.noreply.github.com>
2022-05-31 12:18:44 +0200

    Update Vehicle.cs

--- LearnInheritance/Vehicle.cs
+++ LearnInheritance/Vehicle.cs
@@ -26,21 +26,29 @@ namespace LearnInheritance
             LicensePlate = Tools.GenerateLicensePlate();
         }
 
+        //Dette er en metode for å øke farten, eller verdien på Speed som er det den faktisk gjør.
+        //Her bruker vi "virtual" fordi at en metode som er "virtual" kan man endre(override) ved bruk av "override" i de klassene som arver fra klassen vi har "virtual" i.
         public virtual void SpeedUp()
         {
+            //Øker helt enkelt veriden(i dette tilfellet farten) med 5.
             Speed += 5;
         }
 
+        //Denne metoden fungerer helt likt som SpeedUp(), men i stedet synker veriden/farten med x.
         public virtual void SlowDown()
         {
             Speed -= 5;
         }
 
+        //Dette er en metode som når den blir kalt bare printer ut HONK! (Den blir riktig nok ikke kalt i programmet nå, men var med i koden fra Codecademy)
         public void Honk()
         {
             Console.WriteLine("HONK!");
         }
 
+        //Forklaring på abstract fra W3Schools.
+        //Abstract method: can only be used in an abstract class, and it does not have a body. The body is provided by the derived class (inherited from).
+        //Så denne metoden er tom her i hovedklassen, og forskjellig i hver av de andre klassene.
         public abstract string Describe();
 
     }
